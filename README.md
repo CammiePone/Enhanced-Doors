10cf0da28c8ea1a9f0271907d288cddfcff8ab78
CammiePone <inazuma261@gmail.com>
2024-12-20 13:09:55 -0800

    idk why the gif isnt working :/

---
+++ README.md
@@ -0,0 +1,16 @@
+
+---
+
+Enhanced Doors makes it so doors are now animated and coupled together when opening or closing them.
+
+<div align="center">
+    <img src="https://mod-assets.upcraft.dev/promo/enhanced-doors/banner.gif" width="480" alt="">
+</div>
+
+---
+
+<p align="center">
+	<a href="https://cammiescorner.dev/discord"><img src="https://cammiescorner.dev/images/extras/discord.png" width="150" height="150" title="Join my Discord" alt="The Discord icon, which is a blue circle with half in shadow. In the middle is a weird little robot-helmet-looking-thingy."></a>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="https://www.ko-fi.com/camellias"><img src="https://cammiescorner.dev/images/extras/kofi.png" width="150" height="150" title="Support me on Ko-Fi" alt="The Ko-Fi logo, a sky-blue circle with a white coffee mug with a red heart on it in the middle."></a>
+</p>
--- src/main/java/dev/cammiescorner/enhanceddoors/mixin/DoorBlockMixin.java
+++ src/main/java/dev/cammiescorner/enhanceddoors/mixin/DoorBlockMixin.java
@@ -4,6 +4,7 @@ import dev.cammiescorner.enhanceddoors.common.GotAnyGrapes;
 import dev.cammiescorner.enhanceddoors.common.blocks.entities.DoorBlockEntity;
 import dev.cammiescorner.enhanceddoors.common.registries.EnhancedDoorsComponents;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
@@ -15,8 +16,8 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -31,26 +32,80 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DoorBlock.class)
 public abstract class DoorBlockMixin extends Block implements EntityBlock, GotAnyGrapes {
 	@Shadow @Final public static EnumProperty<DoubleBlockHalf> HALF;
+	@Shadow @Final public static EnumProperty<DoorHingeSide> HINGE;
+	@Shadow @Final public static DirectionProperty FACING;
+	@Shadow @Final public static BooleanProperty OPEN;
+	@Shadow @Final public static BooleanProperty POWERED;
+
+	@Shadow protected abstract void playSound(@Nullable Entity entity, Level level, BlockPos blockPos, boolean bl);
+
+	@Shadow public abstract boolean isOpen(BlockState blockState);
+
 	@Unique private final ThreadLocal<Boolean> A = ThreadLocal.withInitial(() -> false);
 
 	public DoorBlockMixin(Properties properties) { super(properties); }
 
 	// TODO couple the doors pls
 
-	@Inject(method = "neighborChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/DoorBlock;playSound(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Z)V"))
+	@Inject(method = "neighborChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
 	private void openSesameRedstone(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl, CallbackInfo ci) {
+		Direction facing = blockState.getValue(FACING);
+		BlockPos offset = blockPos.relative(blockState.getValue(HINGE) == DoorHingeSide.RIGHT ? facing.getCounterClockWise() : facing.getClockWise());
+		BlockState offsetState = level.getBlockState(offset);
+
+		if(offsetState.getBlock() instanceof DoorBlock && offsetState.getValue(OPEN) == blockState.getValue(OPEN)) {
+			offsetState = offsetState.cycle(POWERED).cycle(OPEN);
+			level.setBlock(offset, offsetState, Block.UPDATE_CLIENTS);
+			playSound(null, level, offset, offsetState.getValue(OPEN));
+			level.gameEvent(null, isOpen(offsetState) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, offset);
+
+			if(level.getBlockEntity(offset.below(offsetState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
+				blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
+
+			if(level.getBlockEntity(offset.below(offsetState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
+				blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
+		}
+
 		if(level.getBlockEntity(blockPos.below(blockState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
 			blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
 	}
 
-	@Inject(method = "setOpen", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/DoorBlock;playSound(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Z)V"))
+	@Inject(method = "setOpen", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
 	private void openSesameVillagers(Entity entity, Level level, BlockState blockState, BlockPos blockPos, boolean bl, CallbackInfo ci) {
+		Direction facing = blockState.getValue(FACING);
+		BlockPos offset = blockPos.relative(blockState.getValue(HINGE) == DoorHingeSide.RIGHT ? facing.getCounterClockWise() : facing.getClockWise());
+		BlockState offsetState = level.getBlockState(offset);
+
+		if(offsetState.getBlock() instanceof DoorBlock && offsetState.getValue(OPEN) == blockState.getValue(OPEN)) {
+			offsetState = offsetState.cycle(OPEN);
+			level.setBlock(offset, offsetState, Block.UPDATE_CLIENTS);
+			playSound(entity, level, offset, offsetState.getValue(OPEN));
+			level.gameEvent(entity, isOpen(offsetState) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, offset);
+
+			if(level.getBlockEntity(offset.below(offsetState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
+				blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
+		}
+
 		if(level.getBlockEntity(blockPos.below(blockState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
 			blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
 	}
 
-	@Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/DoorBlock;playSound(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Z)V"))
+	@Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
 	private void openSesamePlayers(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+		Direction facing = blockState.getValue(FACING);
+		BlockPos offset = blockPos.relative(blockState.getValue(HINGE) == DoorHingeSide.RIGHT ? facing.getCounterClockWise() : facing.getClockWise());
+		BlockState offsetState = level.getBlockState(offset);
+
+		if(offsetState.getBlock() instanceof DoorBlock && offsetState.getValue(OPEN) != blockState.getValue(OPEN)) {
+			offsetState = offsetState.cycle(OPEN);
+			level.setBlock(offset, offsetState, Block.UPDATE_CLIENTS);
+			playSound(player, level, offset, offsetState.getValue(OPEN));
+			level.gameEvent(player, isOpen(offsetState) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, offset);
+
+			if(level.getBlockEntity(offset.below(offsetState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
+				blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
+		}
+
 		if(level.getBlockEntity(blockPos.below(blockState.getValue(HALF) == DoubleBlockHalf.UPPER ? 1 : 0)) instanceof DoorBlockEntity blockEntity)
 			blockEntity.getComponent(EnhancedDoorsComponents.OPENING_PROGRESS).justOpened();
 	}

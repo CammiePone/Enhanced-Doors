49925dde0d882ad256d09bb257a8350178d59353
CammiePone <inazuma261@gmail.com>
2024-12-29 01:38:47 -0800

    now we should be good on servers yay

--- src/main/java/dev/cammiescorner/enhanceddoors/common/components/OpeningProgressComponent.java
+++ src/main/java/dev/cammiescorner/enhanceddoors/common/components/OpeningProgressComponent.java
@@ -3,21 +3,32 @@ package dev.cammiescorner.enhanceddoors.common.components;
 import dev.cammiescorner.enhanceddoors.EnhancedDoorsConfig;
 import dev.cammiescorner.enhanceddoors.common.registries.EnhancedDoorsComponents;
 import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
+import dev.onyxstudios.cca.api.v3.component.tick.ClientTickingComponent;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-public class OpeningProgressComponent implements AutoSyncedComponent {
+public class OpeningProgressComponent implements AutoSyncedComponent, ClientTickingComponent {
 	private final BlockEntity door;
 	private long openingTicks = 0;
+	private long prevOpeningTicks = 0;
 
 	public OpeningProgressComponent(BlockEntity door) {
 		this.door = door;
 	}
 
 	@Override
+	public void clientTick() {
+		prevOpeningTicks = openingTicks;
+
+		if(openingTicks > 0)
+			openingTicks--;
+	}
+
+	@Override
 	public void readFromNbt(CompoundTag tag) {
 		openingTicks = tag.getLong("OpenedAt");
+		prevOpeningTicks = openingTicks;
 	}
 
 	@Override
@@ -29,12 +40,11 @@ public class OpeningProgressComponent implements AutoSyncedComponent {
 		if(!EnhancedDoorsConfig.animateDoors)
 			return 1f;
 
-		long gameTime = door.getLevel().getGameTime();
-		return Mth.clamp((Mth.lerp(tickDelta, gameTime - 1, gameTime) - openingTicks) / EnhancedDoorsConfig.animationLength, 0f, 1f);
+		return 1 - Mth.clamp(Mth.lerp(tickDelta, prevOpeningTicks, openingTicks) / (float) EnhancedDoorsConfig.animationLength, 0f, 1f);
 	}
 
 	public void justOpened() {
-		openingTicks = door.getLevel().getGameTime();
+		openingTicks = EnhancedDoorsConfig.animationLength;
 		door.syncComponent(EnhancedDoorsComponents.OPENING_PROGRESS);
 	}
 }
